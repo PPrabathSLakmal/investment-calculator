76d8412755e3e135c8636705c1faa58fe7c3af00
PPrabathSLakmal <japprabathsanjeewa@gamil.com>
2025-03-27 22:38:12 +0530

    Done CSS modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'investment-calculator';
+
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.scss'
+  styleUrl: './user-input.component.scss',
 })
 export class UserInputComponent {
-
+  enterdInitialInvestment = '0';
+  enteredAnualInvestment = '0';
+  enteredExpectedReturn = '0';
+  enteredDuration = '0';
+  onSubmit() {
+    console.log('submitted');
+  }
 }
